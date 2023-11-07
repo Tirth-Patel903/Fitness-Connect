ff03f8b338e1036e57780e08ff39449c847815c1
parag.extendrum@outlook.com <99315530+Tirth-Patel903@users.noreply.github.com>
2023-11-07 12:48:04 +0530

    New Changes UI and Home Page

--- FitnessConnect/Areas/Identity/Data/Contact.cs
+++ FitnessConnect/Areas/Identity/Data/Contact.cs
@@ -8,6 +8,7 @@ namespace FitnessConnect.Areas.Identity.Data
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public string Subject { get; set; }
         public string Message { get; set; }
     }
 }
